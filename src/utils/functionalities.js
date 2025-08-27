27dc613e3148af6acfd37697e5f1e4db281a8648
Frank Anthony <frankez478@gmail.com>
2025-08-27 11:24:24 +0100

    installed supabase dependency, tried implementing it

--- src/utils/functionalities.js
+++
@@ -1,4 +0,0 @@
-// Utility function for class names
-export function cn(...args) {
-  return args.filter(Boolean).join(" ");
-}
---
+++ src/utils/supabase.js
@@ -0,0 +1,5 @@
+import {createClient} from '@supabase/supabase-js'
+const supabaseURL = import.meta.env.VITE_SUPABASE_URL
+const supabaseAPI = import.meta.env.VITE_SUPABASE_API
+
+export const supabase = createClient(supabaseURL, supabaseAPI)
